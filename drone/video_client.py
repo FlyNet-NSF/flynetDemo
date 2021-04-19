d6273fa27759d1129391a5fd988ad59ccf623d8b
Komal Thareja <kthare10@renci.org>
2021-04-19 14:54:36 -0400

    yolo client container and deploy

--- drone/video_client.py
+++ drone/video_client.py
@@ -1,77 +1,93 @@
+#!/usr/bin/env python3
 import requests
-import math
 import cv2
 import numpy as np
 import time
 import os
 from argparse import ArgumentParser
 
-class video_client(object):
-        def __init__(self, url, outputfile):
-                self.url = url
-                self.outputfile = outputfile
 
-if __name__=="__main__":
-        parser = ArgumentParser(description="video_client")
+if __name__ == "__main__":
+    parser = ArgumentParser(description="video_client")
 
-        parser.add_argument("-u", "--url", metavar="URL", type=str, help="URL to send video data for processing", required=True)
-        parser.add_argument("-o", "--outputfile", metavar="OUTPUT_FILE", type=str, help="Path to output video file", required=True)
+    parser.add_argument("-u", "--url", metavar="URL", type=str, help="URL to send video data for processing",
+                        required=True)
+    parser.add_argument("-i", "--input_dir", metavar="INPUT_DIR", type=str, help="Path to input video directory",
+                        required=True)
+    parser.add_argument("-o", "--output_dir", metavar="OUTPUT_DIR", type=str, help="Path to output directory",
+                        required=True)
 
-        args = parser.parse_args()
-        url = args.url
-        outputfile = args.outputfile
+    args = parser.parse_args()
+    url = args.url
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+
+    frameCount = 1
+    peakFPS = 0
+    minFPS = 10000
+    averageFPS = 0
+    FPS = []
+    startTime = time.time()
+    uri_init = f"http://{url}/init"
+    uri = f"http://{url}/frameProcessing"
+
+    print(f"uri_init={uri_init}")
+    print(f"uri={uri}")
+    processed_files = []
+
+    while True:
+        file_list = os.listdir(input_dir)
+        # Retry to check if there are any new files to be processed every 60 secs.
+        if file_list is None or len(file_list) < 1:
+            time.sleep(60)
         
-        frameCount = 1
-        peakFPS = 0
-        minFPS = 10000
-        averageFPS =0
-        FPS = []
-        startTime = time.time()
-        video = cv2.VideoCapture(outputfile)
-        
-        uri_init = "http://" + url + "/init"
-        uri = "http://" + url + "/frameProcessing"
-        #force 640x480 webcam resolution
-        video.set(3,640)
-        video.set(4,480)
-        response = requests.post(uri_init, json = {})
-        
-        for i in range(0,20):
+        if file_list == processed_files:
+            time.sleep(60)
+
+        for video_file in os.listdir(input_dir):
+            if video_file in processed_files:
+                continue
+            video_file_path = f"{input_dir}/{video_file}"
+            print(f"Processing file: {video_file_path}")
+            file2 = open(f"{output_dir}/result.txt", "a")
+            file2.write(f"Processing file: {video_file_path}\n")
+            file2.close()
+
+            video = cv2.VideoCapture(video_file_path)
+
+            # force 640x480 webcam resolution
+            video.set(3, 640)
+            video.set(4, 480)
+            response = requests.post(uri_init, json={})
+
+            for i in range(0, 20):
                 (grabbed, frame) = video.read()
-            
-        while True:
+
+            while True:
                 frameStartTime = time.time()
-                frameCount+=1
+                frameCount += 1
                 (grabbed, frame) = video.read()
-                height = np.size(frame,0)
-                width = np.size(frame,1)
 
-                #if cannot grab a frame, this program ends here.
+                # if cannot grab a frame, this program ends here.
                 if not grabbed:
-                        break
-                #cv2.imwrite("Frame.jpg", frame)
-                #print(frame.shape)
-                data = {"Frame":frame.tolist()}
-                r = requests.post(uri, json = data)
-                currentFPS = 1.0/(time.time() - frameStartTime)
+                    break
+
+                height = np.size(frame, 0)
+                width = np.size(frame, 1)
+
+                # cv2.imwrite("Frame.jpg", frame)
+                # print(frame.shape)
+                data = {"Frame": frame.tolist()}
+                r = requests.post(uri, json=data)
+                currentFPS = 1.0 / (time.time() - frameStartTime)
                 FPS.append(currentFPS)
                 print("response = {}, frame = {}, fps = {} ".format(r, frameCount, round(currentFPS, 3)))
-                file2 = open("/home/drone/result.txt", "a")
-                file2.write("response = {}, frame = {}, fps = {} ".format(r, frameCount, round(currentFPS, 3)))
-                file2.close
+                file2 = open(f"{output_dir}/result.txt", "a")
+                file2.write("response = {}, frame = {}, fps = {}\n".format(r, frameCount, round(currentFPS, 3)))
+                file2.close()
                 if r == "<Response [500]>":
-                        break
+                    break
                 print("Average FPS = {}".format(round(np.mean(FPS), 3)))
                 print("RunTimeInSeconds = {}".format(round(frameStartTime - startTime, 3)))
 
-if __name__=="__main__":
-        parser = ArgumentParser(description="feature_ensemble")
-        
-        parser.add_argument("-u", "--url", metavar="URL", type=str, help="URL to query for feature collection", required=True)
-        parser.add_argument("-r", "--runs", metavar="RUNS", type=int, help="Number of runs", required=True)
-        parser.add_argument("-o", "--outputfile", metavar="OUTPUT_FILE", type=str, help="Path to output geojson file", required=True)
-        
-        args = parser.parse_args()
-        url = args.url
-        runs = args.runs
-        outputfile = args.outputfile
+            processed_files.append(video_file)
