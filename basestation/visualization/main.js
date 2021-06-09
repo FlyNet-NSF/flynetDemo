6ee97addfe24aa2a0847be7daffe47012692be80
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-06-09 22:25:07 +0000

    svg testing

--- basestation/visualization/main.js
+++ basestation/visualization/main.js
@@ -1,4 +1,5 @@
-var latlonFactor = 3000;  // 100px per 0.1 lat or 0.1 lon
+var latlonFactor = 3000;  // 100px per 0.1 lat or 0.1 lon, also determines initial zoom
+var $canvas = $('#drawboard');
 
 function getPixelsFromLat(lat, drone_lat) {
     var lat_delta = drone_lat - lat;
@@ -34,6 +35,9 @@ function setDroneHeading(heading) {
 
     var drone = $("#drone_icon > img");
     drone.css("transform", "rotate(" + heading + "deg)")
+
+    var drone_wrapper = $("#drone_icon");
+    drone_wrapper.attr("data-heading", heading);
 }
 
 var drone_lat_last = 0;
@@ -43,6 +47,9 @@ function setDroneLat(lat) {
     var drone_info = $("#drone_info .lat_input");
     drone_info.text(lat);
 
+    var drone_wrapper = $("#drone_icon");
+    drone_wrapper.attr("data-lat", lat);
+
     populateLatLines(lat);
 }
 
@@ -51,6 +58,9 @@ function setDroneLon(lon) {
     drone_lon_last = lon;
     var drone_info = $("#drone_info .lon_input");
     drone_info.text(lon);
+
+    var drone_wrapper = $("#drone_icon");
+    drone_wrapper.attr("data-lon", lon);
 
     populateLonLines(lon);
 }
@@ -87,29 +97,49 @@ function populateLonLines(drone_lon) {
     }
 }
 
-function addTower(tower_lat, tower_lon) {
-    tower_width_pxl = getPixelsFromLat(tower_lat, drone_lat_last) * -1;
-    tower_height_pxl = getPixelsFromLon(tower_lon, drone_lon_last) * -1;
+function addTower(tower_lat, tower_lon, id) {
+    tower_width_pxl = getPixelsFromLat(tower_lat, drone_lat_last);
+    tower_height_pxl = getPixelsFromLon(tower_lon, drone_lon_last);
 
-    var tower = "<div style='left: " + tower_width_pxl + "; top: " + tower_height_pxl + ";' class='ctower clearable'><img src='assets/tower.png'></div>"
+    var tower = "<div data-id='" + id + "' data-lat='" + tower_lat + "' data-lon='" + tower_lon + "' style='left: " + tower_width_pxl + "; top: " + tower_height_pxl + ";' class='ctower clearable'><img src='assets/tower.png'></div>"
 
     addDroneObject(tower);
 }
 
-function addGroundStation(station_lat, station_lon) {
-    station_width_pxl = getPixelsFromLat(station_lat, drone_lat_last) * -1;
-    station_height_pxl = getPixelsFromLon(station_lon, drone_lon_last) * -1;
+function addGroundStation(station_lat, station_lon, id) {
+    station_width_pxl = getPixelsFromLat(station_lat, drone_lat_last);
+    station_height_pxl = getPixelsFromLon(station_lon, drone_lon_last);
 
-    var tower = "<div style='left: " + station_width_pxl + "; top: " + station_height_pxl + ";' class='ctower clearable'><img src='assets/station.png'></div>"
+    var station = "<div data-id='" + id + "' data-lat='" + station_lat + "' data-lon='" + station_lon + "' style='left: " + station_width_pxl + "; top: " + station_height_pxl + ";' class='gstation clearable'><img src='assets/station.png'></div>"
 
-    addDroneObject(tower);
+    addDroneObject(station);
+}
+
+function addPathByID(source_id, dest_id) {
+    var source_elem = $("div").find(`[data-id='${source_id}']`);
+    var dest_elem = $("div").find(`[data-id='${dest_id}']`);
+
+    if (source_id == "drone") {
+        source_elem = $("#drone_icon");
+    }
+
+    if (dest_id == "drone") {
+        dest_elem = $("#drone_icon");
+    }
+
+    var drone = $("#drone")
+    var drone_pos = drone.position();
+
+    var source_pos = source_elem.position();
+
+    var dest_pos = dest_elem.position();
 }
 
 function addPath(source_lat, source_lon, dest_lat, dest_lon) {
-    source_width_pxl = getPixelsFromLat(source_lat);
-    source_height_pxl = getPixelsFromLon(source_lon);
-    dest_width_pxl = getPixelsFromLat(dest_lat);
-    dest_height_pxl = getPixelsFromLon(dest_lon);
+    source_width_pxl = getPixelsFromLat(source_lat, drone_lat_last);
+    source_height_pxl = getPixelsFromLon(source_lon, drone_lon_last);
+    dest_width_pxl = getPixelsFromLat(dest_lat, drone_lat_last);
+    dest_height_pxl = getPixelsFromLon(dest_lon, drone_lon_last);
 
     var svg_width = Math.abs(dest_width_pxl - source_width_pxl);
     var svg_height = Math.abs(dest_height_pxl - source_height_pxl);
@@ -135,7 +165,7 @@ function addPath(source_lat, source_lon, dest_lat, dest_lon) {
         y2 = svg_height;
     }
 
-    var svg_script = "<svg width='" + svg_width + " height='" + svg_height + "''>";
+    var svg_script = "<svg width='" + svg_width + "' height='" + svg_height + "'>";
     svg_script += "<line x1='" + x1 + "' y1='" + y1 + "' x2='" + x2 + "' y2='" + y2 + "' style='stroke:rgb(255,0,0);stroke-width:2' />";
     svg_script += "</svg>";
 
@@ -162,19 +192,23 @@ setInterval(function() {
                             break;
                         case "celltowers":
                             $.each(data["drone"][key], function(key, val) {
-                                addTower(val["latitude"], val["longitude"]);
+                                addTower(val["latitude"], val["longitude"], val["id"]);
                             });
                     }
                 });
             } else if (key == "stations") {
                 // ground stations
                 $.each(data[key], function(key, val) {
-                    addGroundStation(val["latitude"], val["longitude"]);
+                    addGroundStation(val["latitude"], val["longitude"], val["id"]);
                 });
             } else if (key == "weights") {
                 // generated graph data
                 $.each(data[key], function(key, val) {
-                    
+                    source_key = key;
+                    $.each(data["weights"][key], function(key, val) {
+                        console.log(val[0]);
+                        addPathByID(source_key, val[0]);
+                    });
                 });
             }
         });
