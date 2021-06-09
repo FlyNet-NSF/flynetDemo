1b5f6e7d958b14060ffdcbd32bb7f78f4ac874c5
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-06-09 17:25:35 +0000

    range circle, drone, and drone info done

--- basestation/visualization/main.js
+++ basestation/visualization/main.js
@@ -1,5 +1,49 @@
-$.getJSON('../../data/file.json', function(data) {         
-    alert(data);
+function clearObjects() {
+    $(".object").remove();
+}
 
-    setTimeout(arguments.callee, 10000);
-});
+function setDroneHeading(heading) {
+    var drone_info = $("#drone_info .head_input");
+    drone_info.text(heading);
+
+    var drone = $("#drone > img");
+    drone.css("transform", "rotate(" + heading + "deg)")
+}
+
+function setDroneLat(lat) {
+    var drone_info = $("#drone_info .lat_input");
+    drone_info.text(lat);
+}
+
+function setDroneLon(lon) {
+    var drone_info = $("#drone_info .lon_input");
+    drone_info.text(lon);
+}
+
+function addTower(heading) {
+
+}
+
+setInterval(function() { 
+    $.getJSON('state.json', function(data) {
+        clearObjects();
+    
+        $.each(data, function (key, val) {
+            if (key == "drone") {
+                $.each(data[key], function(key, val) {
+                    switch(key) {
+                        case "latitude":
+                            setDroneLat(val);
+                            break;
+                        case "longitude":
+                            setDroneLon(val);
+                            break;
+                        case "heading":
+                            setDroneHeading(val)
+                            break;
+                    }
+                });
+            }
+        });
+    });
+}, 5000);
--- basestation/visualization/visualization.py
+++ basestation/visualization/visualization.py
@@ -1,25 +1,13 @@
 import os
-import http.sevrer
+import http.server
+import socketserver
 
-def main():
-    baseconnection = pika.BlockingConnection(pika.ConnectionParameters(host=args.basestation_host, virtual_host=args.basestation_vhost, credentials=credentials))
-    basechannel = baseconnection.channel()
-    basechannel.queue_declare(queue=args.basestation_queue, durable=True)
-  
-    droneconnection = pika.BlockingConnection(pika.ConnectionParameters(host=args.drone_host, virtual_host=args.drone_vhost, credentials=credentials))
-    dronechannel = droneconnection.channel()
-    dronechannel.queue_declare(queue=args.drone_queue, durable=True)
+PORT = 8000
 
-def loop(base_channel, drone_channel):
-    def update_base():
-        
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    def update_drone():
+Handler = http.server.SimpleHTTPRequestHandler
 
-
-    basechannel.basic_consume(queue=args.basestation_queue, auto_ack=False, on_message_callback=callback)
-
-if __name__ == '__main__':
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    http.server()
-    loop()
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
