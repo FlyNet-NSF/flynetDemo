202727c8a8644ffde5ded8768206d02e79192c2a
elyons <elyons@engin.umass.edu>
2021-06-30 18:17:15 +0000

    further improved upon the geojson format that gets sent to the basestation

--- drone/sendToBasestation/sendToBasestation.py
+++ drone/sendToBasestation/sendToBasestation.py
@@ -39,7 +39,9 @@ def main(args):
   currentBattery = random.randint(50, 100)
   endless = 0
 
-  cell_towers = {}
+  drone_flights = [] #in case we want more than one
+  cell_towers = []
+  
   droneData = {}
   droneData['type'] = "Feature"
   droneData['properties'] = {}
@@ -47,6 +49,8 @@ def main(args):
   droneData['properties']['classification'] = "ongoingFlight"
   droneData['properties']['userProperties'] = {}
   droneData['properties']['userProperties']['celltowers'] = {}
+  droneData['properties']['userProperties']['celltowers']['type'] = "FeatureCollection"
+  droneData['properties']['userProperties']['celltowers']['features']= []
   droneData['geometry'] = {}
   droneData['geometry']['type'] = "LineString"
   droneData['geometry']['coordinates'] = []
@@ -74,8 +78,8 @@ def main(args):
     drone_point = Point(prevLat, prevLon, currentAlt)
 
     cell_towers = generateCellTowers(drone_point, cell_towers)  # regenerate cell towers
-
-    for id,tower in cell_towers.items():
+    
+    for tower in cell_towers:
       towerDistanceCalc = Geodesic.WGS84.Inverse(currentLat, currentLon, tower['geometry']['coordinates'][1], tower['geometry']['coordinates'][0])
       towerDistance = towerDistanceCalc['s12']
       rtt = towerDistance / 1000  # calculate RTT with some randomness (factor of distance)
@@ -85,22 +89,29 @@ def main(args):
         bw = round(random.random() * 35)  # in mb/s
         tower['properties']['bandwidth'] = bw
 
-    droneData['properties']['userProperties']['celltowers'] = cell_towers
+    droneData['properties']['userProperties']['celltowers']['features'] = cell_towers
+    drone_flights = [] #clear out the list of flights for now... ideally we'd just update a flight already existing in the list
+    drone_flights.append(droneData)
 
+    drones = {}
+    drones['type'] = "FeatureCollection"
+    drones['features'] = []
+    drones['features'] = drone_flights
+    
     # battery simulation
     currentBattery = currentBattery - 0.1
 
     #print(droneData)
     #droneMessage = str(droneData, 'utf-8')
     #droneMessage = droneData
-    submitToBasestation(args, basechannel, droneData)
+    submitToBasestation(args, basechannel, drones)
 
     time.sleep(5)
   
   print("Flight is complete.  Exiting")
   sys.exit()
   
-def generateCellTowers(location, existing = {}):
+def generateCellTowers(location, existing = []):
   mincount = 4
   distance_limit = 10000  # meters from drone to cell tower
   out = existing
@@ -124,32 +135,28 @@ def generateCellTowers(location, existing = {}):
       longitude = new_tower.longitude
       latitude = new_tower.latitude
       key = "ct_" + str(longitude) + "_" + str(latitude)
-      out[key] = {}
-      out[key]['type'] = "Feature"
-      out[key]['geometry'] = {}
-      out[key]['geometry']['type'] = "Point"
+      this_feature = {}
+      this_feature['type'] = "Feature"
+      this_feature['geometry'] = {}
+      this_feature['geometry']['type'] = "Point"
       this_tuple = [longitude, latitude, 0]
-      out[key]['geometry']['coordinates'] = this_tuple
-      out[key]['properties'] = {}
-      out[key]['properties']['classification'] = "celltower"
-      out[key]['properties']['name'] = key
-      out[key]['properties']['network'] = random.choice(networks)
-      
+      this_feature['geometry']['coordinates'] = this_tuple
+      this_feature['properties'] = {}
+      this_feature['properties']['classification'] = "celltower"
+      this_feature['properties']['name'] = key
+      this_feature['properties']['network'] = random.choice(networks)
+      out.append(this_feature)
   else:
-    delList = []
     # remove out of range cell towers
-    for id,tower in existing.items():
+    for tower in existing:
       tower_lon = tower['geometry']['coordinates'][0]
       tower_lat = tower['geometry']['coordinates'][1]
 
       drone_tower_distance = Geodesic.WGS84.Inverse(location.latitude, location.longitude, tower_lat, tower_lon)['s12']  # calculate distance
 
       if drone_tower_distance > distance_limit:
-        delList.append(id)
+        existing.remove(tower)
 
-    
-    for id in delList:
-      del existing[id]
 
     # add stations if needed
     for i in range(getGenCount()):
@@ -162,16 +169,16 @@ def generateCellTowers(location, existing = {}):
       longitude = new_tower.longitude
       latitude = new_tower.latitude
       key = "ct_" + str(longitude) + "_" + str(latitude)
-      out[key] = {}
-      out[key]['type'] = "Feature"
-      out[key]['geometry'] = {}
-      out[key]['geometry']['type'] = "Point"
+      this_feature = {}
+      this_feature['type'] = "Feature"
+      this_feature['geometry'] = {}
+      this_feature['geometry']['type'] = "Point"
       this_tuple = [longitude, latitude, 0]
-      out[key]['geometry']['coordinates'] = this_tuple
-      out[key]['properties'] = {}
-      out[key]['properties']['name'] = key
-      out[key]['properties']['network'] = random.choice(networks)
-
+      this_feature['geometry']['coordinates'] = this_tuple
+      this_feature['properties'] = {}
+      this_feature['properties']['name'] = key
+      this_feature['properties']['network'] = random.choice(networks)
+      out.append(this_feature)
   return out
 
 def handleArguments(properties):
